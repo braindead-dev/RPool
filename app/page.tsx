c8be08c5a128394bbdd8a6f6c68bcbd23f4159b1
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-11-16 11:07:22 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 export default function Home() {
   return (
     <main className="min-h-screen">
+
+      <section className="py-20 px-4 text-center">
+        <h1 className="text-6xl font-bold mb-6">
+          <div className="text-white-500">Make Friends, Save Gas</div>
+        </h1>
+      </section>
     </main>
   );
 }
