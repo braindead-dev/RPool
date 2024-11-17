245b73abd153817891fd3358765f442b766cca87
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-11-16 17:10:05 -0800

    Update page.tsx

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -20,18 +20,6 @@ export default function Events() {
           </h1>
         </div>
       </section>
-=======
-    return (
-      <main className="min-h-screen">
-        <section className="py-20" style={{
-        background: "url(https://dining.ucr.edu/sites/default/files/header-ucrdining_2.jpg)"
-      }}>
-          <div className="flex">
-            <h1 className="text-6xl font-bold">
-              <div className="indent-80 text-white">All Events</div>
-            </h1>
-          </div>
-        </section>
         
         <section className="py-16 px-4 max-w-7xl mx-auto">
           <div className="flex flex-col space-y-5 ...">
@@ -43,18 +31,6 @@ export default function Events() {
             <EventGrid3 />
           </div>
         </section>
->>>>>>> 4f946bc2ccda7ed1778c5a731cd33a394b37019f
-
-      <section className="py-16 px-4 max-w-7xl mx-auto">
-        <div className="flex flex-col space-y-5 ...">
-          <h2 className="text-3xl font-bold text-gray-900">Featured Events</h2>
-          <EventGrid />
-          <h2 className="text-3xl font-bold text-gray-900">Clubs</h2>
-          <EventGrid />
-          <h2 className="text-3xl font-bold text-gray-900">Local</h2>
-          <EventGrid />
-        </div>
-      </section>
     </main>
   );
 }
